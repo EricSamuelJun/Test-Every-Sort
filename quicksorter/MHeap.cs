da07c9a4a357f4ccfab9475e24c7273b4dbea25d
전성욱 <mam675@naver.com>
2018-12-21 18:32:46 +0900

    힙 한거 추가중. 힙스터임 ㅇㅇ

---
+++ quicksorter/MHeap.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quicksorter {
+    class MHeap {
+        public Mnode header;
+        public virtual void Add() {
+
+        }
+        public virtual int Pop() {
+            return 0;
+        }
+    }
+    class MaxHeap : MHeap {
+        public override void Add() {
+
+        }
+        public override int Pop() {
+            return 0;
+        }
+
+    }
+    class MinHeap :MHeap{
+        public override void Add() {
+
+        }
+        public override int Pop() {
+            return 0;
+        }
+
+    }
+    class Mnode {
+        public Mnode LNode;
+        public Mnode RNode;
+        public Mnode MNode;
+        public int data;
+    }
+}
